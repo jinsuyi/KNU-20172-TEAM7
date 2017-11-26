e7e1ad663e7e127ffe583189219df16e89136f3e
gowls4023 <gowls4023@naver.com>
2017-11-26 15:37:05 +0900

    main source

---
+++ main/chat_time_main.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <curses.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+int row, col;
+int count = 0;
+
+int set_ticker(int );
+
+void main_screen(void );
+void chat_menu(void );
+void time_menu(void );
+void exiting(void );
+
+int main()
+{
+	int c;
+
+	initscr(); // 초기화
+	crmode();
+	noecho();
+	clear();
+
+	main_screen();	// 메인 화면
+
+	while(1) // 'Q'입력 시 종료, '1'입력 시 1번메뉴, '2'입력 시 2번 메뉴
+	{
+		c = getchar();
+		if(c == 'Q'){
+			clear();
+			break;
+		}
+		if(c=='1'){ // 채팅 메뉴 
+			clear();
+			chat_menu();
+			while(1){ // 'B'입력 시 메인메뉴, '1'입력 시 1번메뉴, '2'입력 시 2번 메뉴
+				c= getchar();
+				if(c=='1'){ 
+					clear();
+					refresh();					
+				}
+				if(c=='2'){ 
+					clear();
+					refresh();					
+				}
+				if(c=='B'){
+					clear();
+					main_screen();
+					break;
+				}
+                        }
+		}
+		if(c=='2'){ // 시간표 메
+			clear();
+			time_menu();
+			while(1){ // 'B'입력 시 메인메뉴, '1'입력 시 1번메뉴, '2'입력 시 2번 메뉴
+				c= getchar();
+				if(c=='1'){ 
+					clear();
+					refresh();					
+				}
+				if(c=='2'){ 
+					clear();
+					refresh();					
+				}
+				if(c=='B'){
+					clear();
+					main_screen();
+					break;
+				}
+                        }
+		}
+	}
+
+	exiting();  // 종료 화면
+
+	endwin(); // 종료
+
+	return 0;
+}
+
+void main_screen(void ) // 메인메뉴
+{
+	move(1, 2);
+	addstr("******************* Chatting and Timetable **********************");
+	move(2, 2);
+	addstr(">> press key : '1' : Chatting, '2' : Time Table, 'Q' : exit");	
+
+	move(5, 2);
+	addstr("         1. Chatting");
+
+	move(8, 2);
+	addstr("         2. TimeTable");
+
+	move(13, 25);
+	addstr("*** System Programming - TEAM7 ***");
+	
+	move(14, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(15, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(16, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(17, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(18, 25);
+	addstr("**********************************");
+	
+	refresh();
+}
+
+void chat_menu() // 채팅 메뉴
+{
+	move(1, 2);
+	addstr("******************* Chatting - Create or Enter ******************");
+	move(2, 2);
+	addstr(">> press key : '1' : Create, '2' : Enter, 'B' : back");	
+
+	move(5, 2);
+	addstr("         1. Create in chat room");
+
+	move(8, 2);
+	addstr("         2. Enter the chat room");
+
+	move(13, 25);
+	addstr("*** System Programming - TEAM7 ***");
+	
+	move(14, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(15, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(16, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(17, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(18, 25);
+	addstr("**********************************");
+	
+	refresh();
+}
+
+void time_menu()  // 시간표 메뉴
+{
+	move(1, 2);
+	addstr("******************* TimeTable - Notify or Modify ****************");
+	move(2, 2);
+	addstr(">> press key : '1' : Notify, '2' : Modify, 'B' : back");	
+
+	move(5, 2);
+	addstr("         1. Notify my timetable");
+
+	move(8, 2);
+	addstr("         2. Modify my timetable");
+
+	move(13, 25);
+	addstr("*** System Programming - TEAM7 ***");
+	
+	move(14, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(15, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(16, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(17, 25);
+	addstr("*       2014105078 Lee Hae-JIn   *");
+	move(18, 25);
+	addstr("**********************************");
+	
+	refresh();
+}
+
+void exiting(void ) // 종료 시 화면
+{
+	int delay = 200;
+	void move_msg(int );
+
+	mvaddstr(8, 13, "Exiting program");
+	row=8, col= 28;
+
+	signal(SIGALRM, move_msg);
+	set_ticker(delay);
+	
+	while(1){
+		if (count == 10)
+			break;
+	}
+}
+
+void move_msg(int signum) // 종료화면 디테일
+{
+	signal(SIGALRM, move_msg);
+	mvaddstr(row, col, ".");	
+	count++;
+	col++;			
+	refresh();		
+}
