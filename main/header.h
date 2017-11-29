acd1baa1af3b0bb16e4e96fa76f5b7c30f08de41
gowls4023 <gowls4023@naver.com>
2017-11-30 05:21:57 +0900

    merge header files

---
+++ main/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <curses.h>
+#include <termios.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
